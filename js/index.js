b159dac0d354394e186d83af74bf43179a9e54ec
Marcos Henrique <wakeupmh@gmail.com>
2019-06-27 10:54:20 -0300

    adjusted the flexibility and arrangement

--- js/index.js
+++ js/index.js
@@ -1,21 +1,39 @@
 const consult = {
-    getData:()=>{
-        data.map(i=>{
-            consult.listData(i.id, i.foto, i.nome, i.cargo);
-        });
+    getData:(id)=>{
+        if(id){
+            let finded = data.find(x => x.id === id);
+            consult.listDialog(finded.id, finded.foto, finded.nome, finded.cargo, finded.idade)
+        }
+        else{
+            data.map(i=>{
+                consult.listItems(i.id, i.foto, i.nome, i.cargo);
+            });
+        }
     },
-    listData:(id, foto, nome, cargo)=>{
+    listItems:(id, foto, nome, cargo)=>{
         let txt = ` <div class="img" style="background-image:url(imgs/${foto})">`
-                + ` <div class="badge">`
-                + `     <div>${id}</div>`
-                + ` </div></div>`
+                + `     <div class="badge">`
+                + `         <div>${id}</div>`
+                + `     </div>`
+                + ` </div>`
                 + ` <div class="ctnText">`
                 + `     <strong>${nome}</strong>`
                 + `     <span>${cargo}</span>`
                 + ` </div>`;
-        consult.appendToMyChild('.container', txt, undefined, 'items');
-        
+        handlers.appendToMyChild('.container', txt, undefined, 'items');
     },
+    listDialog:(id, foto, nome, cargo, idade)=>{
+        let txt = ` <div class="img" style="background-image:url(imgs/${foto})">`
+                + ` </div>`
+                + ` <div class="dlgText">`
+                + `     <span>Nome:  <strong>${nome}</strong></span>`
+                + `     <span>Cargo: <strong>${cargo}</strong></span>`
+                + `     <span>Idade: <strong>${idade}</strong></span>`
+                + ` </div>`;
+        handlers.appendToMyChild('dialog', txt, undefined, 'dlgContainer');
+    }
+}
+const handlers = {
     appendToMyChild:(_dad, _html, _nodeElement = 'div', _class)=>{
         let wrapper = document.createElement(_nodeElement);
         wrapper.innerHTML = _html;
@@ -24,4 +42,5 @@ const consult = {
         document.querySelector(_dad).appendChild(wrapper);
     }
 }
+consult.getData(1);
 consult.getData();
